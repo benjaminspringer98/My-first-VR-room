b72eb329c7d4e0117bc095a5488662e5964adc15
benjaminspringer98 <benjamin.springer98@gmail.com>
2022-12-06 20:01:04 +0100

    Initial commit

---
+++ Assets/Clock.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Clock : MonoBehaviour
+
+{
+    public GameObject hourHand;
+    public GameObject minuteHand;
+    public GameObject secondHand;
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateHand();
+    }
+
+    void UpdateHand()
+    {
+        hourHand.transform.localRotation = Quaternion.Euler((System.DateTime.Now.Hour / 12f) * 360f + 90, 0, -90);
+        minuteHand.transform.localRotation = Quaternion.Euler((System.DateTime.Now.Minute / 60f) * 360f + 90, 0, -90);
+        secondHand.transform.localRotation = Quaternion.Euler((System.DateTime.Now.Second / 60f) * 360f + 90, 0, -90);
+    }
+
+}
